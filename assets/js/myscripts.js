9b17f876244edaebd647bfac9b33d4fb2aecec4d
Lillian Chan <lilchan717@gmail.com>
2021-06-21 21:51:17 -0500

    The color coding works. Working on saving and retrieving entered text.

--- assets/js/myscripts.js
+++ assets/js/myscripts.js
@@ -10,13 +10,11 @@ $(".currentHour").text(currentHourEl.format("hh:" + "mm" + "a"));
 console.log(currentHourEl);
 
 
-
-
-
 function rowColor() {
     var currentHourEl = currentDayEl.hours();
     var slot = $(".time-block");
-    for (var i = 0; i < slot.length; i++); {
+    console.log('slot', slot);
+    for (var i = 0; i < slot.length; i++) {
         var rows = slot[i];
         if (parseInt(rows.id.split("-")[0]) < currentHourEl) {
             $(rows).addClass("past");
@@ -34,6 +32,42 @@ function rowColor() {
 rowColor();
 
 // function appointments() {
-    //can save by //clicking save button to //local storage
-    //when refresh, still there
+//can save by //clicking save button to //local storage
+//when refresh, still there
+
+//  $("saveBtn").on("click", function(save) {
+//     save.
+
+$(document).ready(function () {
+    // var editAppt = $(".saveBtn").text();
+    $(".editAppt").click(function () {
+        var str = $(".editAppt").val();
+        localStorage.setItem("str", "");
+        console.log(str);
+    })
+});
+
+
+
+
+// $('.col-md-10 editAppt').val(col-md-10 editAppt);
+
+// console.log(editAppt);
+
+// //Retrieve the col-md-10 editAppt
+
+// function displayVals() {
+//     $("value").val(siblings(".btn col-md-1 saveBtn"))
+//     displayVals() ;
+// }
+
+
+
+// localStorage, input,text, value
+
+// $("#7am".textarea).val(localStorage.getItem("7am"));
+// document.getElementById("input").text = localStorage.getItem("");
+
+    // $(this).val(siblings(".rows"))
+    // console.log("input", alskjdflasjfoiawjeofiwe)
 
